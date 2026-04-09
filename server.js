b4f71ca7ca2ff2da61c9d2a80e6b40904a262f78
Maximous Darks <maximousdx@gmail.com>
2026-04-09 03:12:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from backend!" });
+});
+
 const PORT = process.env.PORT || 3000;
-
-
-app.use(express.static('public'));
-
-
-app.get('/api', (req, res) => {
-    res.json({ message: 'Backend Launched Successfully.' });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
